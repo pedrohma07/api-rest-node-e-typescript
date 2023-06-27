0d8217fea7dc418e29cfeca312e30e556d92a161
pedrohma07 <pedrohma07@gmail.com>
2023-06-27 12:29:05 -0300

    feat: add validation

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,16 +1,14 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import { CidadesController } from './../controllers';
+
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
   return res.send('Olá, DEV');
 });
 
-router.post('/teste', (req, res) => {
-  console.log(req);
-  
-  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post('/cidades', CidadesController.create);
 
 export { router };
